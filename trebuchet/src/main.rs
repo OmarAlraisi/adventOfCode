cb5e49ac51b09bc22b7b962fa370fc1bac4f8700
AlRaisi96 <o.raisi@hotmail.com>
2023-12-04 11:47:30 +0400

    solves advent of code day 1 part 1

--- trebuchet/src/main.rs
+++ trebuchet/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input.txt").unwrap();
+
+    let lines = input.split('\n').into_iter();
+    let mut sum = 0;
+    lines.for_each(|line| {
+        let first = line.chars().find(|x| x.is_digit(10));
+        let last = line.chars().rev().find(|x| x.is_digit(10));
+
+        match (first, last) {
+            (Some(f), Some(l)) => {
+                sum += f.to_digit(10).unwrap() * 10 + l.to_digit(10).unwrap();
+            },
+            _ => ()
+        }
+    });
+
+    println!("{}", sum);
 }
